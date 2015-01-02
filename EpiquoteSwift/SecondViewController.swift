8cf897f81b0b4dc5f9bb0c0c77476bf31662ff68
oppenheimer <maxime.cattet@epita.fr>
2015-01-02 12:55:23 +0100

    Initial Commit

---
+++ EpiquoteSwift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EpiquoteSwift
+//
+//  Created by Maxime Cattet on 02/01/2015.
+//
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
